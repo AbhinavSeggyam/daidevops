afdb4df994bce990d419322a3cedc2c966228c86
Abhinav <abhiseggyam@gmail.com>
2024-12-19 12:31:38 +0530

    Revert "avg, mult, sub"
    This reverts commit 09f59f3203b3017144156084462ce22455c70904.

--- addition.py
+++ addition.py
@@ -1,6 +1,6 @@
 a, b = int(input("Enter 2 numbers: "))
+<<<<<<< HEAD
+
+=======
 print(f'Sum: {a+b}')
-print(f"Product: {a*b}")
-print(f"Difference: {a-b}")
-print(f"Average: {a+b//2}")
-print(f"Max: {max(a,b)}")
+>>>>>>> parent of 09f59f3 (avg, mult, sub)
